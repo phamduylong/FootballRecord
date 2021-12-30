69c51dcaa501cb53c55f7d3b206507a877aa4c93
Duy Long Pham <longphamduy2002@gmail.com>
2021-12-30 20:51:53 +0200

    Changed main to avoid memory leak

--- App.cpp
+++ App.cpp
@@ -1,7 +1,27 @@
 #include "FootballGame.h"
 #include "GameRecords.h"
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 int main(int argc, char* argv[]) {
-	GameRecords recordbook;
-	recordbook.run();
+	_CrtMemState s1;
+	_CrtMemCheckpoint(&s1);
+
+	GameRecords* recordbook = new GameRecords;
+	recordbook->run();
+	delete recordbook;
+	_CrtMemState s2, s3;
+	_CrtMemCheckpoint(&s2);
+	if (_CrtMemDifference(&s3, &s1, &s2)) {
+		std::cout << "Memory leaked\n";
+		_CrtDumpMemoryLeaks();
+		_CrtMemDumpStatistics(&s3);
+	}
+	else {
+		std::cout << "No memory leaked\n";
+	}
+
+
 	return 0;
 }
